8e7322c4c3e9a402409e024cbabd2c18ce185cde
AveiaPodre <av_jr@outlook.com.br>
2023-01-03 14:17:03 -0300

    Cadastro da disciplina feito

---
+++ cadastro.py
@@ -0,0 +1,34 @@
+menu = '''--- Mini Controle Acadêmico ---
+1. Cadastrar disciplina
+2. Pesquisar disciplina
+3. Listar disciplinas cadastradas
+4. Cadastrar professor em disciplina
+5. Matricular aluno em disciplina
+6. Lançar notas do aluno em uma disciplina
+7. Listar alunos de uma disciplina
+8. Listar notas dos alunos de uma disciplina
+9. Sair'''
+
+def cadastra_disc():
+def pesquisa_disc():
+def cadastra_prof():
+def pesquisa_prof():
+def cadastra_aluno():
+def pesquisa_aluno():
+def lista_prof():
+def lista_aluno():
+def insere_notas():
+
+while(True):
+    print(menu)
+    resp = int(input())
+    if(resp == 1):
+    elif(resp == 2):
+    elif(resp == 3):
+    elif(resp == 4):
+    elif(resp == 5):
+    elif(resp == 6):
+    elif(resp == 7):
+    elif(resp == 8):
+    elif(resp == 9):
+        break
